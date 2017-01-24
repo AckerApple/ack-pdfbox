4b86ed6f343868ee659d0deab7ed2c2807570779
ackerapple <acker.dawn.apple@gmail.com>
2017-01-24 15:09:11 -0500

    checkbox fix

--- ackpdfbox/src/main/java/com/ackpdfbox/app/FieldFiller.java
+++ ackpdfbox/src/main/java/com/ackpdfbox/app/FieldFiller.java
@@ -70,7 +70,7 @@ public class FieldFiller{
         pdField.setReadOnly(false);
       }
 
-      if(pdField.getClass().getName()=="org.apache.pdfbox.pdmodel.interactive.form.PDCheckBox" && value!=null && value.getAsString().length()>0 && value.getAsString()!="OFF"){
+      if(pdField.getClass().getName()=="org.apache.pdfbox.pdmodel.interactive.form.PDCheckBox" && value!=null && value.getAsString().length()>0 && value.getAsString().toLowerCase()!="off"){
         PDCheckBox pDCheckBox = (PDCheckBox) acroForm.getField(fullname);
         pDCheckBox.check();
       }else if(value!=null){
